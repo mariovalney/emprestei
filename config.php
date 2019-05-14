87de08ce79678af614be03625730b559795c032b
Mário Valney <mariovalney@gmail.com>
2019-05-14 08:33:14 -0300

    It's alive

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * You know what to do...
+ */
+
+/** Site Config **/
+define('SITE_NAME', 'Emprestei');
+
+/** URL Config **/
+define('BASE_URL', 'http://dev.emprestei.com.br');
+
+/**
+ * Database configs
+ * To deactivate the use of Database, exclude these 4 lines or set DB_NAME as empty: define('DB_NAME', '');
+ */
+
+define('DB_NAME', 'bancodedados');
+define('DB_USER', 'bancodedados');
+define('DB_PASS', 'bancodedados');
+define('DB_HOST', 'bancodedados');
+
+/** Timezone config **/
+date_default_timezone_set('America/Fortaleza');
+
+/** Debug config **/
+define('DEBUG', true);
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Index: receive all the requests and do the Magic...
+ */
+
+/**
+ * Define the ROOT
+ */
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__) . DS);
+define('CORE_DIR', ROOT . DS . 'core');
+define('APP_DIR', ROOT . DS . 'app');
+
+/**
+ * Turn on all the errors: it's nice to developers
+ */
+if (defined('DEBUG') && DEBUG) {
+    error_reporting(E_ALL);
+}
+
+/** Reading the configurations **/
+if (! file_exists(ROOT . 'config.php')) {
+    die('I can\'t find the <code>config.php</code> file. <br> Please, check the file or create a new one.');
+}
+
+require_once(ROOT . 'config.php');
+
+/** Loading Core **/
+require_once(ROOT . 'vendor' . DS . 'autoload.php');
